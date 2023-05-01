359881b548914a714339618371c2456862f05105
gimminjae <min356812@naver.com>
2023-05-01 15:51:56 +0900

    Test: modify post

--- bo/src/test/java/com/bo/post/PostServiceTests.java
+++ bo/src/test/java/com/bo/post/PostServiceTests.java
@@ -28,17 +28,18 @@ public class PostServiceTests {
     @InjectMocks
     private PostService postService;
 
-    private PostDto initData() {
+    private PostDto initData(int num) {
         return PostDto.builder()
-                        .title("title1")
-                        .content("content1")
-                        .build();
+                .title("title%d".formatted(num))
+                .content("content%d".formatted(num))
+                .build();
     }
+
     @DisplayName("create Post, then get Post by postId")
     @Test
     void test1() {
         //given
-        PostDto postDto = initData();
+        PostDto postDto = initData(1);
 
         //when
         postService.create(postDto);
@@ -48,5 +49,40 @@ public class PostServiceTests {
         //then
         assertThat(resultDto.getTitle()).isEqualTo("title1");
         assertThat(resultDto.getContent()).isEqualTo("content1");
+    }
+
+    @DisplayName("get post by author")
+    @Test
+    void test3() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @DisplayName("modify post")
+    @Test
+    void test2() {
+        //given
+        PostDto postDto = initData(1);
+        postService.create(postDto);
+        PostDto resultDto = postService.getById(1L);
+
+        assertThat(resultDto.getTitle()).isEqualTo("updated title");
+        assertThat(resultDto.getContent()).isEqualTo("updated content");
+
+        //when
+        when(postRepository.findById(any())).thenReturn(Optional.of(Post.from(postDto)));
+        PostDto updatedDto = PostDto.builder()
+                .title("updated title")
+                .content("updated content")
+                .build();
+        postService.update(updatedDto);
+        resultDto = postService.getById(1L);
+
+        //then
+        assertThat(resultDto.getTitle()).isEqualTo("updated title");
+        assertThat(resultDto.getContent()).isEqualTo("updated content");
     }
 }
